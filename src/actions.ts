fa12515c67e77778325aca4f747d4a7bfdf686f8
Kristin Gorge <gorgekristin@gmail.com>
2020-09-21 21:30:57 -0700

    make sure there isn't jquery loaded in the sw

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,6 @@
 import $ from "jquery";
 import { orchestrator } from "./progress";
+import { log } from "./logger";
 import { DownloadableState } from "./downloadable_item";
 import * as SampleDownloads from "./sample_downloadable_items";
 import { MANAGER } from "./download_manager";
@@ -7,6 +8,10 @@ import { MANAGER } from "./download_manager";
 
 export function addClickHandlers() {
     $("#action-single").click(async (e) => {
+        if (!('BackgroundFetchManager' in self)) {
+            log('browser does not support background fetch :(');
+            return;
+        }
         const dl = new DownloadableState(SampleDownloads.SINGLE);
         await dl.startDownload();
 
@@ -15,6 +20,10 @@ export function addClickHandlers() {
 
     // Fetches 50 ~1.5 MB video segments.
     $("#action-multiple").click(async (e) => {
+        if (!('BackgroundFetchManager' in self)) {
+            log('browser does not support background fetch :(');
+            return;
+        }
         const dl = new DownloadableState(SampleDownloads.MULTI);
         await dl.startDownload();
 
